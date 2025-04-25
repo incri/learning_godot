ee782047cd044d7bc44987dd3e1ee675ed1bd9e2
incri <paudelbaibhav0@gmail.com>
2025-04-25 21:22:02 +0545

    added movement animation , jump animation and ideal animation

---
+++ scripts/astrael.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_jumping := false
+var is_moving := false
+var is_running := false
+
+func _process(_delta: float) -> void:
+	# Get input direction
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
+	# Determine movement state
+	is_moving = input_dir.length() > 0.1
+	
+	# Handle animation transitions
+	if is_jumping:
+		animation_player.play("Player/jump")
+	elif is_moving:
+		animation_player.play("Player/run")
+	else:
+		animation_player.play("Player/ideal")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		is_jumping = true
+	elif event.is_action_released("jump"):
+		is_jumping = false 
