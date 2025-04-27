5a703639601d2f3e6573e04eb063afc0b65295f8
incri <paudelbaibhav0@gmail.com>
2025-04-27 23:28:22 +0545

    added blended ideal jump and running jump animation with locomotion

--- scripts/player_animation.gd
+++ scripts/player_animation.gd
@@ -3,12 +3,17 @@ extends Node3D
 @export var animationTree: AnimationTree
 
 @export var locomotionBlendPath: String
+@export var jumpmotionBlendPath: String
+@export var locojumpBlendPath: String
 
 @export var player_movements: Node  
 
 var input_dir = Vector2.ZERO
 var current_blend: float = 0.0
 var blend_speed: float = 2.0
+var locojump_blend: float = 0.0
+var jumpmotion_blend: float
+var jump_timer: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,8 +30,26 @@ func _process(_delta: float) -> void:
 			else:
 				target_blend = -1.0
 		current_blend = lerp(current_blend, target_blend, blend_speed * _delta)
-		print("Computed blend value: ", current_blend)
 		animationTree.set(locomotionBlendPath, current_blend)
+		if Input.is_action_just_pressed("jump"):
+			print("pressed jump")
+			locojump_blend = 1.0
+			if target_blend > 0.6 or target_blend < -0.6:
+				jumpmotion_blend = 1.0
+				jump_timer = 0.5
+			else:
+				jumpmotion_blend = 0.0
+			animationTree.set(locojumpBlendPath, locojump_blend)
+			animationTree.set(jumpmotionBlendPath, jumpmotion_blend)
+		if jump_timer > 0:
+			jump_timer -= _delta
+		else:
+			if locojump_blend > 0:
+				locojump_blend = lerp(locojump_blend, 0.0, blend_speed * _delta)
+				animationTree.set(locojumpBlendPath, locojump_blend)
+			if jumpmotion_blend > 0:
+				jumpmotion_blend = lerp(jumpmotion_blend, 0.0, blend_speed * _delta)
+				animationTree.set(jumpmotionBlendPath, jumpmotion_blend)
 	else:
 		print("AnimationTree is not set") 
 
