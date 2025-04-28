5d67af02ef473ee0eeb5729568e3d150ee02e04b
incri <paudelbaibhav0@gmail.com>
2025-04-28 22:55:19 +0545

    refactor player movement removing unwanted code

--- scripts/player_movements.gd
+++ scripts/player_movements.gd
@@ -1,27 +1,17 @@
 extends CharacterBody3D
 
-@export var turn_speed: float = 5.0
-@export var run_speed: float = 8.0
-@export var gravity: float = 30.0
-@export var jump_speed: float = 12.0
-@export var sprint_speed: float = 14.0
-@export var dash_speed: float = 20.0
-@export var dash_duration: float = 0.2
-@export var dash_cooldown_time: float = 1.0
+@export var turn_speed: float = 4.0
+@export var run_speed: float = 4.0
+@export var jump_height: float = 6.0
+@export var sprint_speed: float = 8.0
+@export var gravity: float = 9.8
 
 var astrael: Node3D
 var camera: Camera3D
 var orientation = Transform3D()
 var can_double_jump: bool = false
-var dash_timer: float = 0.0
-var dash_cooldown_timer: float = 0.0
-var is_dashing: bool = false
-var has_air_dashed: bool = false
 
-var jump_start_time: float = 0.0
-var is_jumping: bool = false
-var total_air_time_estimate: float = 0.0
-var jump_phase: int = 0
+
 
 signal input_dir_updated(new_dir: Vector2)
 signal jump_phase_changed(phase: int)
@@ -31,102 +21,38 @@ func _ready():
 	camera = $CameraPivot/SpringArm3D/Camera3D
 
 func _physics_process(delta):
-	var input_dir = Vector2.ZERO
-	if Input.is_action_pressed("move_forward"):
-		input_dir.y -= 1
-	if Input.is_action_pressed("move_backward"):
-		input_dir.y += 1
-	if Input.is_action_pressed("move_left"):
-		input_dir.x -= 1
-	if Input.is_action_pressed("move_right"):
-		input_dir.x += 1
-
-	if input_dir != Vector2.ZERO:
-		input_dir = input_dir.normalized()
+	#camera and movement
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	input_dir_updated.emit(input_dir)
+
 	var cam_rot = camera.global_transform.basis.get_euler()
-	
-	var move_dir = Vector3.ZERO
+	var cam_basis = Basis(Vector3.UP, cam_rot.y)
+
 	var current_speed = run_speed
 	if Input.is_action_pressed("sprint"):
 		current_speed = sprint_speed
 
-	if input_dir != Vector2.ZERO:
-		orientation.basis = Basis(Vector3.UP, cam_rot.y)
-		move_dir = orientation.basis.x * input_dir.x + orientation.basis.z * input_dir.y
-		move_dir = move_dir.normalized()
-
-		if Input.is_action_just_pressed("dash") and dash_cooldown_timer <= 0:
-			if is_on_floor():
-				is_dashing = true
-				dash_timer = dash_duration
-				dash_cooldown_timer = dash_cooldown_time
-				has_air_dashed = false
-			elif not is_on_floor() and not has_air_dashed:
-				is_dashing = true
-				dash_timer = dash_duration
-				has_air_dashed = true
-				dash_cooldown_timer = dash_cooldown_time
-
-		if is_dashing:
-			velocity = move_dir * dash_speed
-			velocity.y = 0
-			dash_timer -= delta
-			if dash_timer <= 0:
-				is_dashing = false
-		else:
-			velocity.x = move_dir.x * current_speed
-			velocity.z = move_dir.z * current_speed
-
-		if move_dir.length() > 0.01:
-			astrael.transform = astrael.transform.looking_at(astrael.transform.origin + move_dir, Vector3.UP)
+	if input_dir.length_squared() > 0:
+		var move_dir = (cam_basis.z * input_dir.y + cam_basis.x * input_dir.x).normalized()
+		astrael.look_at(astrael.global_transform.origin - move_dir, Vector3.UP)
+		
+		velocity.x = move_dir.x * current_speed
+		velocity.z = move_dir.z * current_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
+#jumping
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_height
+		jump_phase_changed.emit(0)
 
-	if dash_cooldown_timer > 0:
-		dash_cooldown_timer -= delta
+		can_double_jump = true
+	elif can_double_jump:
+		velocity.y = jump_height
+		jump_phase_changed.emit(0)
+		can_double_jump = false
+	velocity.y -= gravity * delta
+	jump_phase_changed.emit(1)
 
-	if not is_dashing and not is_on_floor():
-		velocity.y -= gravity * delta
-	else:
-		velocity.y = 0
-
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
-			is_jumping = true
-			has_air_dashed = false
-			jump_start_time = Time.get_ticks_msec()
-			jump_phase = 0
-			jump_phase_changed.emit(jump_phase)
-			can_double_jump = true
-		elif can_double_jump:
-			velocity.y = jump_speed
-			is_jumping = true
-			jump_start_time = Time.get_ticks_msec()
-			jump_phase = 0
-			jump_phase_changed.emit(jump_phase)
-			can_double_jump = false
-
-	if not is_on_floor() and is_jumping:
-		var current_time = Time.get_ticks_msec()
-		var air_time = (current_time - jump_start_time) / 1000.0
-		
-		total_air_time_estimate = 2 * jump_speed / gravity
-		var phase_division = total_air_time_estimate / 3.0
-		
-		if air_time > phase_division * 1 and jump_phase < 1:
-			jump_phase = 1
-			jump_phase_changed.emit(jump_phase)
-		elif air_time > phase_division * 2 and jump_phase < 2:
-			jump_phase = 2
-			jump_phase_changed.emit(jump_phase)
-		
-		velocity.y -= gravity * delta
-	
-	if is_on_floor() and is_jumping:
-		is_jumping = false
-		jump_phase = 0
 
 	move_and_slide()
